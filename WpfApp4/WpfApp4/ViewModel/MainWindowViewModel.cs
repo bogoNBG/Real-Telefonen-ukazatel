eaf1738ffc0d74d6547a82bd0b17bdeb363826d1
bogoNBG <bogonbg@gmail.com>
2023-07-10 17:13:33 +0300

    k

---
+++ WpfApp4/WpfApp4/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using WpfApp4.Model;
+using WpfApp4.MVVM;
+
+namespace WpfApp4.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+            Item item = new Item();
+            item.Name = "imence";
+            item.SerialNumber = "ewq123ewq";
+            item.Quantity = 2;
+            Items.Add(item);
+        }
+
+		private Item selectedItem;
+        public Item SelectedItem
+		{
+			get { return selectedItem; }
+			set
+            {
+                selectedItem = value;
+                OnPropertyChanged();
+            }
+		}       
+    }
+}
